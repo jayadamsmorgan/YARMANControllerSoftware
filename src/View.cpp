78b3ef8c13f4b76469b469515c626e99f95d1129
jayadamsmorgan <applejay1802@gmail.com>
2023-11-23 21:11:30 -0500

    Reformat

--- src/View.cpp
+++ src/View.cpp
@@ -6,13 +6,13 @@ View::View(uint8_t width, uint8_t height) : width(width), height(height) {
   pixels = new uint8_t[(width * height + 7) / 8]{};
 }
 
-View::View(uint8_t width, uint8_t height, uint8_t x, uint8_t y) : 
-  width(width), height(height), x(x), y(y) {
+View::View(uint8_t width, uint8_t height, uint8_t x, uint8_t y)
+        : width(width), height(height), x(x), y(y) {
   pixels = new uint8_t[(width * height + 7) / 8]{};
 }
 
-View::View(uint8_t width, uint8_t height, uint8_t pixels[]) :
-  width(width), height(height) {
+View::View(uint8_t width, uint8_t height, uint8_t pixels[])
+        : width(width), height(height) {
   x = 0;
   y = 0;
   this->pixels = (uint8_t*) pixels;
@@ -28,21 +28,18 @@ View::~View() {
 }
 
 View& View::operator+=(const View& src) {
-  // Calculate overlapping area
   uint8_t startX = max(x, src.x);
   uint8_t endX = min(x + width, src.x + src.width);
   uint8_t startY = max(y, src.y);
   uint8_t endY = min(y + height, src.y + src.height);
   for (uint8_t yy = startY; yy < endY; ++yy) {
     for (uint8_t xx = startX; xx < endX; ++xx) {
-      // Calculate the index and bit position in the pixels array
       uint8_t index = yy * width + xx;
       uint8_t bitPos = index % 8;
       index /= 8;
       uint8_t srcIndex = (yy - src.y) * src.width + (xx - src.x);
       uint8_t srcBitPos = srcIndex % 8;
       srcIndex /= 8;
-      // Overwrite the bit
       if (src.pixels[srcIndex] & (1 << srcBitPos)) {
           pixels[index] |= (1 << bitPos);
       } else {
